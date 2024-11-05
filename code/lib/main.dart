5b6b1589ff24143f63cf9c1da8f2b65f101a41c6
NETbrew <113974723+cesarvanleuffelen@users.noreply.github.com>
2024-11-05 09:00:01 +0100

    scroll fix en foto hoogte

--- code/lib/main.dart
+++ code/lib/main.dart
@@ -12,6 +12,7 @@ class ArRondleiding extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'AR Rondleiding',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: const Color(0xFFFFFFFF)
--- code/lib/screens/HomePage.dart
+++ code/lib/screens/HomePage.dart
@@ -7,23 +7,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: InteractiveViewer(
-        boundaryMargin: const EdgeInsets.all(20),
-        minScale: 0.1,
-        maxScale: 5.0,
-        panEnabled: false,
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.85,
+        child: InteractiveViewer(
+          boundaryMargin: const EdgeInsets.all(20),
+          minScale: 0.5,
+          maxScale: 3.0,
 
-        child: Stack(
-          children: [
-            Image.asset(
-              'assets/images/grondplan_rondleiding_groot.jpg', // Place your image in the assets folder
-              fit: BoxFit.cover,
-              height: MediaQuery.of(context).size.height,
-            ),
-            // Buttons placed at specific locations
-            const PositionedButton(index: 1, x: 100, y: 150),
-            // Add more PositionedButton widgets as needed
-          ],
+          child: Stack(
+            children: [
+              Image.asset(
+                'assets/images/grondplan_rondleiding_groot.jpg', // Place your image in the assets folder
+                fit: BoxFit.cover,
+                height: MediaQuery.of(context).size.height,
+              ),
+              // Buttons placed at specific locations
+              const PositionedButton(index: 1, x: 100, y: 150),
+              const PositionedButton(index: 2, x: 200, y: 300),
+              const PositionedButton(index: 3, x: 300, y: 400),
+              // Add more PositionedButton widgets as needed
+            ],
+          ),
         ),
       ),
     );
